57d1f3b9938155bb08d60dbea1862a02593abfdf
MatthewGould123 <matt@saowapan.com>
2025-02-08 14:51:21 +0700

    correct seeding

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -1,12 +1,22 @@
 import Router from "@koa/router";
 import qrcode from "qrcode";
+import { EventAttendeeModel } from "src/database/models/eventAttendee";
 
 const router = new Router({
   prefix: "/rest/auth",
 });
 
 router.get("/", async (ctx) => {
-  const qr = await qrcode.toDataURL("679c448e471d4d80ca6a781c");
+  const model = await EventAttendeeModel.findOne().where({
+    email: "matthew@gould.com",
+  });
+
+  if (!model) {
+    ctx.body = `Could not find model for attendee, oops`;
+    return;
+  }
+
+  const qr = await qrcode.toDataURL(model.id);
 
   ctx.body = `
   <html>
